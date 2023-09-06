5fb50d1e3dcaedf57b5bd53d43e8295ec4f014ca
heart5 <baiyefeng@gmail.com>
2023-09-06 19:39:23 +0800

    功能模块，从everwork直接拷贝调用

---
+++ func/logme.py
@@ -0,0 +1,71 @@
+# encoding:utf-8
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: ipynb,py:percent
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+# ---
+
+# %%
+"""
+构建日志，格式化日志输出内容，限定每个日志文件大小为1M，在25个日志文件内循环
+"""
+
+# %%
+import logging as lg
+import logging.handlers as lgh
+import os
+import pathlib
+
+# %%
+import pathmagic
+with pathmagic.context():
+    from func.first import dirlog, touchfilepath2depth
+
+
+# %%
+def mylog(dirlog):
+    """
+    日志函数，定义输出文件和格式等内容
+    :returns    返回log对象
+    """
+    logew = lg.getLogger('hjer')
+    touchfilepath2depth(dirlog)
+    loghandler = lgh.RotatingFileHandler(str(dirlog), encoding='utf-8',
+                                         # 此处指定log文件的编码方式，否则可能乱码
+                                         maxBytes=1024 * 1024, backupCount=23)
+    formats = lg.Formatter('%(asctime)s\t%(filename)s - [%(funcName)s]'
+                           '\t%(levelname)s: %(message)s',
+                           datefmt='%Y-%m-%d %H:%M:%S')
+    loghandler.setFormatter(formats)
+    logew.setLevel(lg.DEBUG)
+    logew.addHandler(loghandler)
+
+    ################################################################################################
+    # 定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
+    console = lg.StreamHandler()
+    console.setLevel(lg.DEBUG)
+    formatter = lg.Formatter('%(asctime)s\t%(threadName)s - %(thread)d , '
+                             '%(processName)s - %(process)d: '
+                             '%(levelname)-8s %(message)s')
+    console.setFormatter(formatter)
+    lg.getLogger('').addHandler(console)
+    # logew.addHandler(console)
+    ################################################################################################
+
+    return logew
+
+
+# %%
+log = mylog(dirlog)
+
+# %%
+if __name__ == '__main__':
+    cwd = os.getcwd()
+    print(cwd)
+    log.info('测试func下的log，主要看路径')
