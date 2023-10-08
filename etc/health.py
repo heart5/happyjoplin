1a899f0ebd4c425543edf7e8536f01fc882cdd9d
heart5 <baiyefeng@gmail.com>
2023-10-08 23:55:59 +0800

    根据flake8提示去除无用import并修正部分语法

--- etc/health.py
+++ etc/health.py
@@ -21,29 +21,29 @@
 # ### 核心库
 
 # %%
-import os
+# import os
 import re
 import arrow
 import pandas as pd
-import matplotlib
+# import matplotlib
 import base64
 import io
 # from tzlocal import get_localzone
-from threading import Timer
+# from threading import Timer
 
 # %%
 import pathmagic
 with pathmagic.context():
-    from func.first import getdirmain
+    # from func.first import getdirmain
     from func.datetimetools import datecn2utc
     from func.configpr import getcfpoptionvalue, setcfpoptionvalue
-    from func.jpfuncs import getinivaluefromcloud, searchnotes, getnote, createnote, updatenote_imgdata, updatenote_body, updatenote_title, noteid_used
+    from func.jpfuncs import searchnotes, getnote, createnote, updatenote_imgdata, noteid_used
     from func.logme import log
-    from func.wrapfuncs import timethis, ift2phone
+    from func.wrapfuncs import timethis
     # from func.termuxtools import termux_location, termux_telephony_deviceinfo
     # from func.nettools import ifttt_notify
-    from etc.getid import getdeviceid
-    from func.sysfunc import not_IPython, set_timeout, after_timeout, execcmd
+    # from etc.getid import getdeviceid
+    from func.sysfunc import not_IPython, execcmd
 
 # %% [markdown]
 # ### 中文显示预置
@@ -81,8 +81,8 @@ def gethealthdatafromnote(noteid):
 
     itemslist = [[datecn2utc(item[0]), int(item[1]), timestr2minutes(item[2]), item[3]] for item in itemslist0]
 
-    columns=["date", "step", "sleep", "memo"]
-    columns=["日期", "步数", "睡眠时长", "随记"]
+    columns = ["date", "step", "sleep", "memo"]
+    columns = ["日期", "步数", "睡眠时长", "随记"]
     df = pd.DataFrame(itemslist, columns=columns).set_index("日期")
     print(df.dtypes)
     print(type(df.index))
@@ -95,6 +95,7 @@ def gethealthdatafromnote(noteid):
 
 # %%
 def hdf2imgbase64(hdf):
+
     plt.figure(figsize=(16, 20))
 
     ax1 = plt.subplot2grid((4, 2), (0, 0), colspan=2, rowspan=2)
@@ -103,7 +104,7 @@ def hdf2imgbase64(hdf):
     ax1.plot(junhdf, lw=1, label=u'七天日均')
     # 标注数据点
     for i in range(len(junhdf.index)):
-        plt.annotate(f'({int(junhdf.iloc[i])})', (junhdf.index[i], junhdf.iloc[i]), textcoords="offset points", xytext=(0,10), ha='center')
+        plt.annotate(f'({int(junhdf.iloc[i])})', (junhdf.index[i], junhdf.iloc[i]), textcoords="offset points", xytext=(0, 10), ha='center')
     ax1.legend(loc=1)
     ax1.set_title("步数动态图")
 
@@ -113,7 +114,7 @@ def hdf2imgbase64(hdf):
     ax2.plot(sleepjundf, lw=1, label=u'七天平均')
     # 标注数据点
     for i in range(len(sleepjundf.index)):
-        plt.annotate(f'({int(sleepjundf.iloc[i] / 60)}钟{int(sleepjundf.iloc[i] % 60)}分)', (sleepjundf.index[i], sleepjundf.iloc[i]), textcoords="offset points", xytext=(0,10), ha='center')
+        plt.annotate(f'({int(sleepjundf.iloc[i] / 60)}钟{int(sleepjundf.iloc[i] % 60)}分)', (sleepjundf.index[i], sleepjundf.iloc[i]), textcoords="offset points", xytext=(0, 10), ha='center')
     ax2.legend(loc=1)
     ax2.set_title("睡眠时长动态图")
 
@@ -143,6 +144,7 @@ def hdf2imgbase64(hdf):
 # %%
 @timethis
 def health2note():
+
     login_user = execcmd("whoami")
     namestr = "happyjp_life"
     section = f"health_{login_user}"
@@ -150,7 +152,7 @@ def health2note():
     if not (health_id := getcfpoptionvalue(namestr, section, 'health_cloud_id')):
         findhealthnotes = searchnotes("title:健康运动笔记")
         if len(findhealthnotes) == 0:
-            log.critical(f"标题为《健康运动笔记》的笔记貌似不存在，请按照规定格式构建之！退出先！！！")
+            log.critical("标题为《健康运动笔记》的笔记貌似不存在，请按照规定格式构建之！退出先！！！")
             exit(1)
         healthnote = findhealthnotes[0]
         health_id = healthnote.id
@@ -168,7 +170,7 @@ def health2note():
     hdf = gethealthdatafromnote(note.id)
     image_base64 = hdf2imgbase64(hdf)
     if not (healthstat_cloud_id := getcfpoptionvalue(namestr, section, 'healthstat_cloud_id')):
-        healthnotefindlist = searchnotes(f"title:{notestat_title}") 
+        healthnotefindlist = searchnotes(f"title:{notestat_title}")
         if (len(healthnotefindlist) == 0):
             healthstat_cloud_id = createnote(title=notestat_title, imgdata64=image_base64)
             log.info(f"新的健康动态笔记“{healthstat_cloud_id}”新建成功！")
@@ -192,7 +194,8 @@ def health2note():
 if __name__ == '__main__':
     if not_IPython():
         log.info(f'开始运行文件\t{__file__}')
-    
+
     health2note()
+
     if not_IPython():
         log.info(f'Done.结束执行文件\t{__file__}')
