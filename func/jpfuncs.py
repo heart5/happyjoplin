c363682ff530bb554474c523f4059703e922c84d
heart5 <baiyefeng@gmail.com>
2023-11-25 10:34:24 +0800

    模块级别（函数体外）运行getapi并全局调用，优化运行效率

--- func/jpfuncs.py
+++ func/jpfuncs.py
@@ -81,7 +81,7 @@ def getapi():
     url = f"http://localhost:{port}"
     api = Api(token=token, url=url)
 
-    return api, token, port
+    return api
 
 
 # %% [markdown]
@@ -93,10 +93,10 @@ def searchnotebook(title):
     """
     查找指定title（全名）的笔记本并返回id，如果不存在，则新建一个返回id
     """
-    api, token, port = getapi()
-    result = api.search(query=title, type="folder")
+    global jpapi
+    result = jpapi.search(query=title, type="folder")
     if len(result.items) == 0:
-        nbid = api.add_notebook(title=title)
+        nbid = jpapi.add_notebook(title=title)
         log.critical(f"新建笔记本《{title}》，id为：\t{nbid}")
     else:
         nbid = result.items[0].id
@@ -112,9 +112,9 @@ def getallnotes():
     """
     获取所有笔记；默认仅输出id、parent_id和title三项有效信息
     """
-    api = getapi()[0]
+    global jpapi
 
-    return api.get_all_notes()
+    return jpapi.get_all_notes()
 
 
 # %% [markdown]
@@ -122,9 +122,9 @@ def getallnotes():
 
 # %%
 def getnoteswithfields(fields, limit=10):
-    api = getapi()[0]
+    global jpapi
     fields_ls = fields.split(",")
-    allnotes = [note for note in api.get_all_notes(fields=fields)[:limit]]
+    allnotes = [note for note in jpapi.get_all_notes(fields=fields)[:limit]]
     geonotes = [note for note in allnotes if note.altitude != 0 and note.longitude != 0]
     print(len(allnotes), len(geonotes))
     for note in geonotes:
@@ -149,13 +149,13 @@ def getnote(id):
     """
     通过id获取笔记的所有可能内容，NoteData
     """
-    api = getapi()[0]
+    global jpapi
     # 所有可能的属性名称：
     # fields="id, parent_id, title, body, created_time, updated_time, is_conflict, latitude, longitude, altitude, author, source_url, is_todo, todo_due, todo_completed, source, source_application, application_data, order, user_created_time, user_updated_time, encryption_cipher_text, encryption_applied, markup_language, is_shared, share_id, conflict_original_id, master_key_id, body_html, base_url, image_data_url, crop_rect")
     # 经过测试，fields中不能携带的属性值有：
     # latitude, longitude, altitude, master_key_id, body_html,  image_data_url, crop_rect
     fields="id, parent_id, title, body, created_time, updated_time, is_conflict, author, source_url, is_todo, todo_due, todo_completed, source, source_application, application_data, order, user_created_time, user_updated_time, encryption_cipher_text, encryption_applied, markup_language, is_shared, share_id, conflict_original_id"
-    note = api.get_note(id, fields=fields)
+    note = jpapi.get_note(id, fields=fields)
 
     return note
 
@@ -165,7 +165,6 @@ def getnote(id):
 
 # %%
 def noteid_used(targetid):
-    api, token, port = getapi()
     try:
         getnote(targetid)
         return True
@@ -179,9 +178,9 @@ def noteid_used(targetid):
 
 # %%
 def resid_used(targetid):
-    api, token, port = getapi()
+    global jpapi
     try:
-        res = api.get_resource(id_=targetid)
+        res = jpapi.get_resource(id_=targetid)
         return True
     except Exception as e:
         log.info(f"id为{targetid}的资源文件不存在，id号可用。")
@@ -198,14 +197,14 @@ def createnote(title="Superman", body="Keep focus, man!", parent_id=None, imgdat
     按照传入的参数值构建笔记并返回id
     """
 
-    api, token, port = getapi()
+    global jpapi
     if imgdata64:
-        noteid = api.add_note(title=title, image_data_url=f"data:image/png;base64,{imgdata64}")
-        api.modify_note(noteid, body=f"{body}\n{getnote(noteid).body}")
+        noteid = jpapi.add_note(title=title, image_data_url=f"data:image/png;base64,{imgdata64}")
+        jpapi.modify_note(noteid, body=f"{body}\n{getnote(noteid).body}")
     else:
-        noteid = api.add_note(title=title, body=body)
+        noteid = jpapi.add_note(title=title, body=body)
     if parent_id:
-        api.modify_note(noteid, parent_id=parent_id)
+        jpapi.modify_note(noteid, parent_id=parent_id)
     note = getnote(noteid)
     matches = re.findall(r"\[.*\]\(:.*\/([A-Za-z0-9]{32})\)", note.body)
     if len(matches) > 0:
@@ -226,16 +225,16 @@ def createnotewithfile(title="Superman", body="Keep focus, man!", parent_id=None
     按照传入的参数值构建笔记并返回id
     """
 
-    api, token, port = getapi()
+    global jpapi
     if filepath:
-        note_id = api.add_note(title=title)
-        resource_id = api.add_resource(filename=filepath, title=filepath.split("/")[-1])
-        api.add_resource_to_note(resource_id=resource_id, note_id=note_id)
-        api.modify_note(note_id, body=f"{api.get_note(note_id).body}\n{body}")
+        note_id = jpapi.add_note(title=title)
+        resource_id = jpapi.add_resource(filename=filepath, title=filepath.split("/")[-1])
+        jpapi.add_resource_to_note(resource_id=resource_id, note_id=note_id)
+        jpapi.modify_note(note_id, body=f"{jpapi.get_note(note_id).body}\n{body}")
     else:
-        note_id = api.add_note(title=title, body=body)
+        note_id = jpapi.add_note(title=title, body=body)
     if parent_id:
-        api.modify_note(noteid, parent_id=parent_id)
+        jpapi.modify_note(noteid, parent_id=parent_id)
     note = getnote(note_id)
     matches = re.findall(r"\[.*\]\(:.*\/([A-Za-z0-9]{32})\)", note.body)
     if len(matches) > 0:
@@ -252,12 +251,12 @@ def createnotewithfile(title="Superman", body="Keep focus, man!", parent_id=None
 # %%
 @timethis
 def updatenote_title(noteid, titlestr):
-    api = getapi()[0]
+    global jpapi
     note = getnote(noteid)
     titleold = note.title
     if titlestr == titleold:
         return
-    api.modify_note(noteid, title=titlestr)
+    jpapi.modify_note(noteid, title=titlestr)
     log.info(f"笔记《{titleold}》的标题被更新为《{titlestr}》。")
 
 
@@ -267,9 +266,9 @@ def updatenote_title(noteid, titlestr):
 # %%
 @timethis
 def updatenote_body(noteid, bodystr):
-    api = getapi()[0]
+    global jpapi
     note = getnote(noteid)
-    api.modify_note(noteid, body=bodystr)
+    jpapi.modify_note(noteid, body=bodystr)
     log.info(f"笔记《{note.title}》（id：{noteid}）的body内容被更新了。")
 
 
@@ -282,7 +281,7 @@ def updatenote_imgdata(noteid, parent_id=None, imgdata64=None, imgtitle=None):
     """
     用构新去旧的方式更新包含资源的笔记，返回新建笔记的id和资源id列表
     """
-    api = getapi()[0]
+    global jpapi
     note = getnote(noteid)
     origin_body = note.body
     if (origin_body is None) or (len(origin_body) == 0):
@@ -293,26 +292,26 @@ def updatenote_imgdata(noteid, parent_id=None, imgdata64=None, imgtitle=None):
     matches = re.findall(r"\[.*\]\(:.*\/([A-Za-z0-9]{32})\)", origin_body)
     for resid in matches:
         if resid_used(resid):
-            api.delete_resource(resid)
+            jpapi.delete_resource(resid)
             log.critical(f"资源文件（id：{resid}）成功删除。")
         else:
             log.critical(f"资源文件（id：{resid}）不存在，无法删除，跳过。")
-    api.delete_note(noteid)
+    jpapi.delete_note(noteid)
     log.info(f"笔记《{note.title}》（id：{noteid}）中的资源文件{matches}和该笔记都已从笔记系统中删除！")
 
     # notenew_id = api.add_note(title=note.title, image_data_url=f"data:image/png;base64,{imgdata64}")
     notenew_id = createnote(title=note.title, imgdata64=imgdata64)
     if (parent_id is not None) and (parent_id != note.parent_id):
-        api.modify_note(notenew_id, parent_id=parent_id)
-        nb_title = api.get_notebook(parent_id).title
-        nb_old_title = api.get_notebook(note.parent_id).title
+        jpapi.modify_note(notenew_id, parent_id=parent_id)
+        nb_title = jpapi.get_notebook(parent_id).title
+        nb_old_title = jpapi.get_notebook(note.parent_id).title
         log.critical(f"笔记《{note.title}》从笔记本《{nb_old_title}》调整到《{nb_title}》中！")
     notenew = getnote(notenew_id)
     matchesnew = re.findall(r"\[.*\]\(:.*\/([A-Za-z0-9]{32})\)", notenew.body)
     res_id_lst = matchesnew
     if not imgtitle:
         imgtitle = f"happyjoplin {arrow.now()}"
-    api.modify_resource(id_=res_id_lst[0], title=f"{imgtitle}")
+    jpapi.modify_resource(id_=res_id_lst[0], title=f"{imgtitle}")
     log.info(f"构建新的笔记《{note.title}》（id：{notenew_id}）成功，并且构建了新的资源文件{matchesnew}进入笔记系统。")
     print(f"笔记《{notenew.title}》（id：{notenew_id}）的内容为：\t{notenew.body}")
 
@@ -342,12 +341,12 @@ def test_updatenote_imgdata():
 
 # %%
 def explore_resource(res_id):
-    api = getapi()[0]
+    global jpapi
     # fields = ['encryption_blob_encrypted', 'share_id', 'mime', 'updated_time', 'master_key_id', 'is_shared', 'user_updated_time', 'encryption_applied', 'user_created_time', 'size', 'filename', 'file_extension', 'encryption_cipher_text', 'id', 'title', 'created_time']
     # res4test = api.get_resource(res_id, fileds=fields)
-    resnew = api.get_resource(res_id)
+    resnew = jpapi.get_resource(res_id)
     log.info(f"id为{res_id}的资源标题为《{resnew.title}》")
-    res_file = api.get_resource_file(res_id)
+    res_file = jpapi.get_resource_file(res_id)
     log.info(f"id为{res_id}的资源文件大小为{len(res_file)}")
 
 
@@ -360,29 +359,29 @@ def modify_resource(res_id, imgdata64=None):
     """
     试图更新data但是无法成功，暂存之
     """
-    api, token, port = getapi()
-    res = api.get_resource(res_id)
+    global jpapi
+    res = jpapi.get_resource(res_id)
     log.info(f"id为{res_id}的资源标题为《{res.title}》")
-    res_file = api.get_resource_file(res_id)
+    res_file = jpapi.get_resource_file(res_id)
     log.info(f"id为{res_id}的资源文件大小为{len(res_file)}")
     if not imgdata64:
         oldtitle = res.title
         newtitle = "This time is modify time for me."
-        api.modify_resource(id_=res_id, title=newtitle)
+        jpapi.modify_resource(id_=res_id, title=newtitle)
         log.info(f"id为{res_id}的资源标题从《{oldtitle}》更改为《{newtitle}》")
     else:
         datastr = f"data:image/png;base64,{imgdata64}"
         begin_str = f"curl -X PUT -F 'data=\"{datastr}\"'"
         props_str = " -F 'props={\"title\":\"my modified title\"}'"
-        url_str = f" http://localhost:{port}/resources/{res_id}?token={token}"
+        url_str = f" http://localhost:{jpapi.port}/resources/{res_id}?token={jpapi.token}"
         update_curl_str = begin_str + props_str + url_str
         print(update_curl_str)
         outstr = execcmd(update_curl_str)
         log.info(f"{outstr}")
         # api.modify_resource(id_=res_id, data=f"data:image/png;base64,{imgdata64}")
-    resnew = api.get_resource(res_id)
+    resnew = jpapi.get_resource(res_id)
     log.info(f"id为{res_id}的资源标题为《{resnew.title}》")
-    res_file = api.get_resource_file(res_id)
+    res_file = jpapi.get_resource_file(res_id)
     log.info(f"id为{res_id}的资源文件大小为{len(res_file)}")
 
     return res_file
@@ -396,15 +395,15 @@ def getreslst(noteid):
     """
     以字典列表的形式返回输入id笔记包含的资源文件，包含id、title和contentb
     """
-    api, token, port = getapi()
-    dLst = api.get_resources(note_id=noteid)
+    global jpapi
+    dLst = jpapi.get_resources(note_id=noteid)
     # print(type(dLst), dLst)
     reslst = []
     for res in dLst.items:
         sond = dict()
         sond["id"] = res.id
         sond["title"] = res.title
-        sond["contentb"] = api.get_resource_file(res.id)
+        sond["contentb"] = jpapi.get_resource_file(res.id)
         reslst.append(sond)
     # print(reslst[0])
     return reslst
@@ -419,15 +418,15 @@ def searchnotes(key, parent_id=None):
     """
     传入关键字搜索并返回笔记列表，每个笔记中包含了所有可能提取field值
     """
-    api = getapi()[0]
+    global jpapi
     # 经过测试，fields中不能携带的属性值有：latitude, longitude, altitude, master_key_id, body_html,  image_data_url, crop_rect
     # note = api.get_note(id, fields="id, parent_id, title, body, created_time, updated_time, is_conflict, author, source_url, is_todo, todo_due, todo_completed, source, source_application, application_data, order, user_created_time, user_updated_time, encryption_cipher_text, encryption_applied, markup_language, is_shared, share_id, conflict_original_id")
     # note = api.get_note(id, fields="id, latitude, longitude, altitude")
     fields="id, parent_id, title, body, created_time, updated_time, is_conflict, author, source_url, is_todo, todo_due, todo_completed, source, source_application, application_data, order, user_created_time, user_updated_time, encryption_cipher_text, encryption_applied, markup_language, is_shared, share_id, conflict_original_id"
-    results = api.search(query=key, fields=fields).items
+    results = jpapi.search(query=key, fields=fields).items
     log.info(f"搜索“{key}”，找到{len(results)}条笔记")
     if parent_id:
-        nb= api.get_notebook(parent_id)
+        nb= jpapi.get_notebook(parent_id)
         results = [note for note in results if note.parent_id == parent_id]
         log.info(f"限定笔记本《{nb.title}》后，搜索结果有{len(results)}条笔记")
 
@@ -486,6 +485,12 @@ def getinivaluefromcloud(section, option):
 
 
 # %% [markdown]
+# ### 获取jpapi，全局共用
+
+# %%
+jpapi = getapi()
+
+# %% [markdown]
 # ## 主函数main（）
 
 # %%
@@ -494,7 +499,6 @@ if __name__ == '__main__':
         log.info(f'开始运行文件\t{__file__}')
     # joplinport()
 
-    api, token, port = getapi()
     # createnote(title="重生的笔记", body="some things happen", noteid_spec="3ffccc7c48fc4b25bcd7cf3841421ce5")
     test_updatenote_imgdata()
     # test_modify_res()
