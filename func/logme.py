cbd28d0009f45406351b013a73e2bb41e94856fd
heart5 <baiyefeng@gmail.com>
2023-11-28 09:01:37 +0800

    调整logew为loghj

--- func/logme.py
+++ func/logme.py
@@ -34,7 +34,7 @@ def mylog(dirlog):
     日志函数，定义输出文件和格式等内容
     :returns    返回log对象
     """
-    logew = lg.getLogger('hjer')
+    loghj = lg.getLogger('hjer')
     touchfilepath2depth(dirlog)
     loghandler = lgh.RotatingFileHandler(str(dirlog), encoding='utf-8',
                                          # 此处指定log文件的编码方式，否则可能乱码
@@ -43,8 +43,8 @@ def mylog(dirlog):
                            '\t%(levelname)s: %(message)s',
                            datefmt='%Y-%m-%d %H:%M:%S')
     loghandler.setFormatter(formats)
-    logew.setLevel(lg.DEBUG)
-    logew.addHandler(loghandler)
+    loghj.setLevel(lg.DEBUG)
+    loghj.addHandler(loghandler)
 
     ################################################################################################
     # 定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
@@ -58,7 +58,7 @@ def mylog(dirlog):
     # logew.addHandler(console)
     ################################################################################################
 
-    return logew
+    return loghj
 
 
 # %%
@@ -69,3 +69,6 @@ if __name__ == '__main__':
     cwd = os.getcwd()
     print(cwd)
     log.info('测试func下的log，主要看路径')
+
+# %%
+print(log.handlers)
