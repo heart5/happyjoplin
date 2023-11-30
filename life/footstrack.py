c7d69802eac1a4234b99efb7b591c7d8f7ca7d10
heart5 <baiyefeng@gmail.com>
2023-12-01 01:23:23 +0800

    字典化termux-location获取的位置信息

--- life/footstrack.py
+++ life/footstrack.py
@@ -11,25 +11,34 @@
 #       format_version: '1.3'
 # ---
 
-# %%
-"""
-记录足迹
-"""
+# %% [markdown]
+# # GPS位置信息记录
+
+# %% [markdown]
+# ## 引入重要库
 
 # %%
 # from pylab import *
+import datetime
+# import subprocess
 
 import pathmagic
 with pathmagic.context():
-    from func.configpr import getcfpoptionvalue, setcfpoptionvalue
+    from func.configpr import getcfpoptionvalue, setcfpoptionvalue, is_log_details
     from func.first import dirmainpath
     from func.datatools import readfromtxt, write2txt
     from func.logme import log
     from func.wrapfuncs import timethis
     from func.termuxtools import termux_location
-    from etc.getid import getdeviceid
+    from etc.getid import getdeviceid, gethostuser
     from func.sysfunc import set_timeout, after_timeout, not_IPython
 
+
+# %% [markdown]
+# ## 函数库
+
+# %% [markdown]
+# ### foot2record()
 
 # %%
 @set_timeout(240, after_timeout)
@@ -53,27 +62,25 @@ def foot2record():
     itemread = readfromtxt(txtfilename)
     numlimit = 5    # 显示项目数
     print(itemread[:numlimit])
-    tlst = ['datetime', 'latitude', 'longitude', 'altitude', 'accuracy',
-            'bearing', 'speed', 'elapsedMs', 'provider']
     locinfo = termux_location()
     print(locinfo)
     nowstr = datetime.datetime.now().strftime('%F %T')
+    itemnewr = [nowstr]
     if locinfo == False:
-        itemnewr = [f"{nowstr}\t{str(locinfo)}"]
+        itemnewr.extend[f"{str(locinfo)}"]
     else:
-        itemnewr = [f"{nowstr}\t{locinfo[tlst[1]]}\t{locinfo[tlst[2]]}"
-                    f"\t{locinfo[tlst[3]]}\t{locinfo[tlst[4]]}"
-                    f"\t{locinfo[tlst[5]]}\t{locinfo[tlst[6]]}"
-                    f"\t{locinfo[tlst[7]]}\t{locinfo[tlst[8]]}"]
-    itemnewr.extend(itemread)
-    print(itemnewr[:numlimit])
-    write2txt(txtfilename, itemnewr)
+        itemnewr.extend(locinfo.values())
+    itemnewr = [str(x) for x in itemnewr]
+    itemline = ['\t'.join(itemnewr)]
+    itemline.extend(itemread)
+    print(itemline[:numlimit])
+    write2txt(txtfilename, itemline)
 
 
 # %%
 if __name__ == '__main__':
     if not_IPython():
-        log.info(f'运行文件\t{__file__}……')    
+        log.info(f'运行文件\t{__file__}……')
     foot2record()
     if not_IPython():
         print(f"完成文件{__file__}\t的运行")
