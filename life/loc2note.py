72e2cd54fe050f35c34b79466a61009981448b7e
heart5 <baiyefeng@gmail.com>
2025-07-18 21:16:27 +0800

    min和max函数的括号忘了

--- life/loc2note.py
+++ life/loc2note.py
@@ -282,8 +282,8 @@ def update_note_metadata(df, resource_id, location_dict):
         if dev != "total"
     )
     mintime, maxtime = location_dict["metadata"]["time_range"]
-    mintime = mintime if thedf["time"].min > mintime else thedf["time"].min()
-    maxtime = maxtime if thedf["time"].max < maxtime else thedf["time"].max()
+    mintime = mintime if thedf["time"].min() > mintime else thedf["time"].min()
+    maxtime = maxtime if thedf["time"].max() < maxtime else thedf["time"].max()
     location_dict["metadata"]["time_range"] = (mintime, maxtime)
     location_dict["metadata"]["resource_id"] = resource_id
     location_dict["metadata"]["data_file"] = jpapi.get_resource(resource_id).title
