7765b9d2525cf4705072247b0cb1f3a7b070a17a
heart5 <baiyefeng@gmail.com>
2025-10-01 21:28:31 +0800

    测试随手代码片段的文件

---
+++ fortest.py
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     notebook_metadata_filter: jupytext,-kernelspec,-jupytext.text_representation.jupytext_version
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+# ---
+
+# %%
+import pathmagic
+
+with pathmagic.context():
+    from func.first import getdirmain
+
+# %%
+print(getdirmain())
