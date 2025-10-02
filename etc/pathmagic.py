3c9f69b96ffe190b7daac2eec05d080dd4712ab1
heart5 <baiyefeng@gmail.com>
2025-10-02 13:36:46 +0800

    把func子模块搞好的pathmagic.py应用到项目的etc、life、muse和work目录下。

--- etc/pathmagic.py
+++ etc/pathmagic.py
@@ -19,18 +19,33 @@
 
 # %%
 import sys
-
+from pathlib import Path
 
 # %% [markdown]
 # ## context类
 
 
 # %%
-class context:
-    def __enter__(self):
-        sys.path.extend(["..", "."])
+class context:  # noqa: N801
+    """这是一个上下文管理器类，用于临时修改sys.path。"""
 
-    def __exit__(self, *args):
+    def __enter__(self) -> None:
+        """进入上下文管理器时，将当前目录和上级目录添加到sys.path中。"""
+        syspathlst = [Path(p).resolve() for p in sys.path]
+        for inpath in ["..", "."]:
+            if Path(inpath).resolve() not in syspathlst:
+                sys.path.append(inpath)
+        # self.printsyspath()
+
+    @staticmethod
+    def printsyspath() -> None:
+        """打印sys.path中的所有路径。"""
+        for pson in sys.path:
+            print(Path(pson).resolve())
+        print(10 * '*')
+
+    def __exit__(self, *args: any) -> None:
+        """退出上下文管理器时，不做任何操作。"""
         pass
 
 
@@ -39,9 +54,8 @@ class context:
 
 # %%
 if __name__ == "__main__":
-    print(f"运行文件\t{__file__}")
-
+    # print(f"运行文件\t{__file__}")
     for pp in sys.path:
-        print(pp)
-
+        pson = Path(pp).resolve()
+        print(pson)
     print("Done.完毕。")
--- muse/pathmagic.py
+++ muse/pathmagic.py
@@ -11,35 +11,51 @@
 #       format_version: '1.3'
 # ---
 
-# %%
-"""
-功能描述
-"""
-
-# %%
-# import os
-import sys
-
+# %% [markdown]
+# # 魔法路径
 
 # %% [markdown]
-# bp = os.path.dirname(os.path.realpath('.')).split(os.sep)
-# modpath = os.sep.join(bp + ['src'])
-# sys.path.insert(0, modpath)
-# sys.path.insert(0,os.sep.join(bp))
+# ## 引入库
+
+# %%
+import sys
+from pathlib import Path
+
+# %% [markdown]
+# ## context类
 
 
 # %%
-class context:
-    def __enter__(self):
-        sys.path.extend(["..", "."])
+class context:  # noqa: N801
+    """这是一个上下文管理器类，用于临时修改sys.path。"""
 
-    def __exit__(self, *args):
+    def __enter__(self) -> None:
+        """进入上下文管理器时，将当前目录和上级目录添加到sys.path中。"""
+        syspathlst = [Path(p).resolve() for p in sys.path]
+        for inpath in ["..", "."]:
+            if Path(inpath).resolve() not in syspathlst:
+                sys.path.append(inpath)
+        # self.printsyspath()
+
+    @staticmethod
+    def printsyspath() -> None:
+        """打印sys.path中的所有路径。"""
+        for pson in sys.path:
+            print(Path(pson).resolve())
+        print(10 * '*')
+
+    def __exit__(self, *args: any) -> None:
+        """退出上下文管理器时，不做任何操作。"""
         pass
 
 
+# %% [markdown]
+# ## 主函数main()
+
 # %%
 if __name__ == "__main__":
-    print(f"运行文件\t{__file__}")
+    # print(f"运行文件\t{__file__}")
     for pp in sys.path:
-        print(pp)
+        pson = Path(pp).resolve()
+        print(pson)
     print("Done.完毕。")
